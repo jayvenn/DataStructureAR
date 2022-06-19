3bc074b3372e84f3d03179d17d41207fcd5a7a70
Jayven Nhan <nhantrivinh@gmail.com>
2022-06-19 18:42:59 +0800

    Refactor font related files

--- DataStructuresAR/App/Fonts/FontManager.swift
+++ DataStructuresAR/App/Fonts/FontManager.swift
@@ -11,5 +11,4 @@ class FontManager {
   static let shared = FontManager()
   var titleFont: UIFont = Font(object: .titleLabel).instance
   var subtitleFont: UIFont = Font(object: .subTitleLabel).instance
-  
 }
